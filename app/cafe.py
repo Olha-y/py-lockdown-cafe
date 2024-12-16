3c18745897130f0cb7d148fb2419f2d0ccce5a8e
Olha Yukhnenko <pyolh36@gmail.com>
2024-12-16 23:32:01 +0200

    fix_flake8

--- app/cafe.py
+++ app/cafe.py
@@ -1,5 +1,7 @@
 import datetime
-from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
 
 
 class Cafe:
@@ -8,13 +10,19 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"All {visitor['name']} should be vaccinated")
+            raise NotVaccinatedError(
+                f"All {visitor['name']} should be vaccinated"
+            )
 
         expiration_date = visitor["vaccine"]["expiration_date"]
         if expiration_date < datetime.date.today():
-            raise OutdatedVaccineError(f"{visitor['name']}'s vaccine is outdated")
+            raise OutdatedVaccineError(
+                f"{visitor['name']}'s vaccine is outdated"
+            )
 
         if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError(f"{visitor['name']} is not wearing a mask")
+            raise NotWearingMaskError(
+                f"{visitor['name']} is not wearing a mask"
+            )
 
         return f"Welcome to {self.name}"
